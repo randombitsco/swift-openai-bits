1436665c64d805109312417a86375cb4744d541a
David Peterson <david@randombits.org>
2022-09-23 14:33:33 +1000

    Added XCTAssertExpectOpenAICall

--- Sources/OpenAIBitsTestHelpers/ClientHelpers.swift
+++ Sources/OpenAIBitsTestHelpers/ClientHelpers.swift
@@ -3,12 +3,58 @@ import XCTest
 
 //XCTAssertExpectOpenAICall {
 //  Completions(model: "foobar", prompt: "ABC")
+//} returning: {
+//  Completions.Response(...)
 //} whileDoing: {
 //  try await cmd.validate()
 //  try await cmd.run()
 //}
 
-public func XCTAssertExpectOpenAICall<C: Call>(_ call: () -> C, whileDoing: () async throws -> Void) throws {
+enum TestCallError: Swift.Error {
+  case unexpectedCall(Any)
+}
+
+class TestCallHandler<E: Call>: CallHandler {
+  
   var called = false
-  var oldHandler: OpenAIBits.CallHandler
+  let expectedCall: E
+  let returning: E.Response
+  
+  init(expectedCall: E, returning: E.Response) {
+    self.expectedCall = expectedCall
+    self.returning = returning
+  }
+  
+  func execute<C>(call: C, with client: OpenAIBits.Client) async throws -> C.Response where C : OpenAIBits.Call {
+    guard !called else {
+      throw TestCallError.unexpectedCall(call)
+    }
+    guard let call = call as? E else {
+      throw TestCallError.unexpectedCall(call)
+    }
+    called = true
+    guard call == expectedCall else {
+      throw TestCallError.unexpectedCall(call)
+    }
+    return returning as! C.Response
+  }
+}
+
+public func XCTAssertExpectOpenAICall<C: Call>(
+  _ call: () -> C,
+  returning: () -> C.Response,
+  whileDoing: () async throws -> Void,
+  file: StaticString = #file,
+  line: UInt = #line
+) async throws {
+  let oldHandler = Client.handler
+  defer { Client.handler = oldHandler }
+  
+  Client.handler = TestCallHandler(expectedCall: call(), returning: returning())
+  
+  do {
+    try await whileDoing()
+  } catch TestCallError.unexpectedCall(let call) {
+    XCTFail("Unexpected Call: \(call)", file: file, line: line)
+  }
 }
