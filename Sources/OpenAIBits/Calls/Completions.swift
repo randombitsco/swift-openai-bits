2a9851149f085ff7b9c555dfa87bc5b23655e572
David Peterson <david@randombits.org>
2022-10-21 02:42:29 +1000

    Added documentation.

--- Sources/OpenAIBits/Calls/Completions.swift
+++ Sources/OpenAIBits/Calls/Completions.swift
@@ -160,25 +160,50 @@ extension Completions {
 }
 
 extension Completions.Create {
+  /// Represents 1 to 4 "stop" `String` values.
   public struct Stop: Equatable, Encodable {
+    /// The "stop" values.
     public let value: [String]
     
+    /// A single stop value.
+    ///
+    /// - Parameter v1: The only `String`.
     public init(_ v1: String) {
       self.value = [v1]
     }
     
+    /// Two stop values.
+    ///
+    /// - Parameters:
+    ///   - v1: The first `String`.
+    ///   - v2: The second `String`.
     public init(_ v1: String, _ v2: String) {
       self.value = [v1, v2]
     }
     
+    /// Three stop values.
+    ///
+    /// - Parameters:
+    ///   - v1: The first `String`.
+    ///   - v2: The second `String`.
+    ///   - v3: The third `String`.
     public init(_ v1: String, _ v2: String, _ v3: String) {
       self.value = [v1, v2, v3]
     }
     
+    /// Four stop values.
+    ///
+    /// - Parameters:
+    ///   - v1: The first `String`.
+    ///   - v2: The second `String`.
+    ///   - v3: The third `String`.
+    ///   - v4: The fourth `String`.
     public init(_ v1: String, _ v2: String, _ v3: String, _ v4: String) {
       self.value = [v1, v2, v3, v4]
     }
     
+    /// Encodes the this as a `String` array.
+    /// - Parameter encoder: The encoder.
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       try container.encode(value)
