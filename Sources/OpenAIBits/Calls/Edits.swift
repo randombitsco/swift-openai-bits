5f340b98c6f51a1faf5a268d643e7775b6dd309e
David Peterson <david@randombits.org>
2022-10-20 19:25:47 +1000

    Updated documentation.

--- Sources/OpenAIBits/Calls/Edits.swift
+++ Sources/OpenAIBits/Calls/Edits.swift
@@ -24,33 +24,37 @@ extension Edits {
     
     var path: String { "edits" }
     
-    /// The model's ID.
+    /// ID of the model to use. You can use ``Models/List`` to see all of your available models, or see our [Model overview](https://beta.openai.com/docs/models/overview) for descriptions of them.
     public let model: Model.ID
     
-    /// The input string to be edited.
+    /// The input text to use as a starting point for the edit.
     public let input: String?
     
-    /// The instruction for how to edit it.
+    /// The instruction that tells the model how to edit the prompt.
     public let instruction: String
     
-    /// The number of choices to return (defaults to `1`)
+    /// The number of ``Edit/Choice`` values to return. (defaults to `1`)
     public let n: Int?
     
-    /// What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.
+    /// What sampling temperature to use. Higher values means tzhe model will take more risks. Try `0.9` for more creative applications, and `0` (argmax sampling) for ones with a well-defined answer.
+    ///
+    /// We generally recommend altering this or ``topP`` but not both.
     public let temperature: Percentage?
     
-    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So `0.1` means only the tokens comprising the top 10% probability mass are considered.
+    ///
+    /// We generally recommend altering this or ``temperature`` but not both.
     public let topP: Percentage?
     
     /// Creates a new ``Edit`` creation call.
     ///
     /// - Parameters:
-    ///   - model: ID of the model to use. You can use the List models API to see all of your available models, or see our Model overview for descriptions of them.
+    ///   - model: ID of the model to use. You can use ``Models/List`` to see all of your available models, or see our [Model overview](https://beta.openai.com/docs/models/overview) for descriptions of them.
     ///   - input: The input text to use as a starting point for the edit.
     ///   - instruction: The instruction that tells the model how to edit the prompt.
-    ///   - n: The number of ``Edit/Choice`` values to return.
-    ///   - temperature: What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.
-    ///   - topP: An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    ///   - n: The number of ``Edit/Choice`` values to return. (defaults to `1`)
+    ///   - temperature: What sampling temperature to use. Higher values means tzhe model will take more risks. Try `0.9` for more creative applications, and `0` (argmax sampling) for ones with a well-defined answer.
+    ///   - topP: An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So `0.1` means only the tokens comprising the top 10% probability mass are considered.
     ///
     /// - Note: We generally recommend altering `temperature` or `topP` but not both.
     public init(
--- Sources/OpenAIBits/Calls/Embeddings.swift
+++ Sources/OpenAIBits/Calls/Embeddings.swift
@@ -46,7 +46,7 @@ extension Embeddings {
     
     /// Input text to get embeddings for, encoded as a string or array of tokens. To get embeddings for multiple inputs in a single request, pass an array of strings or array of token arrays. Each input must not exceed 2048 tokens in length.
     ///
-    /// Unless you are embedding code, we suggest replacing newlines (`\n`) in your input with a single space, as we have observed inferior results when newlines are present.
+    /// Unless you are embedding code, we suggest replacing newlines (`"\n"`) in your input with a single space, as we have observed inferior results when newlines are present.
     public let input: Input
     
     /// A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse.
