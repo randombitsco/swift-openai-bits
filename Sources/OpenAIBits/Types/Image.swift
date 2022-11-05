e31d80ccae1944c77e9fd9d3a2de8e37cf3d841b
David Peterson <david@randombits.org>
2022-11-05 11:21:35 +1000

    - Fixed Image response structure to match the actual value - Added Base64 data support.

--- Sources/OpenAIBits/Types/Image.swift
+++ Sources/OpenAIBits/Types/Image.swift
@@ -1,40 +1,57 @@
-public enum Image: JSONResponse, Equatable {
-  case url(String)
-  // case base64(Data)
-}
+import Foundation
 
-extension Image {
-
-  enum CodingKeys: String, CodingKey {
-      case url
-      // case base64
+public struct Image: JSONResponse, Equatable {
+  public enum Data: Codable, Equatable {
+    case url(URL)
+    case base64(Foundation.Data)
   }
 
-   /// Decodes an image from a response data. It will be either an object with a `url` field, containing a string with the URL, or a `b64_json` field, containing a string with the base64-encoded string value for the image data.
-   public init(from decoder: Decoder) throws {
-     // get a keyed container
-     let container = try decoder.container(keyedBy: CodingKeys.self)
-     // try to decode the url
-     if let url = try? container.decode(String.self, forKey: .url) {
-       self = .url(url)
-       return
-     }
-     // try to decode the base64
-     // if let base64 = try? container.decode(String.self, forKey: .base64) {
-     //   self = .base64(Data(base64Encoded: base64)!)
-     //   return
-     // }
-     // if we get here, we couldn't decode either
-     throw Client.Error.unexpectedResponse("Unknown response format for image.")
-   }
+  public let created: Date
 
-   public func encode(to encoder: Encoder) throws {
-     var container = encoder.container(keyedBy: CodingKeys.self)
-     switch self {
-     case .url(let url):
-       try container.encode(url, forKey: .url)
-     // case .base64(let data):
-     //   try container.encode(data.base64EncodedString(), forKey: .base64)
-     }
-   }
+  public let data: [Data]
+}
+
+extension Image.Data {
+  
+  public func getData() throws -> Foundation.Data {
+    switch self {
+    case .url(let url):
+      return try Data(contentsOf: url)
+    case .base64(let data):
+      return data
+    }
+  }
+
+  enum CodingKeys: String, CodingKey {
+    case url
+    case base64
+  }
+
+  /// Decodes an image from a response data. It will be either an object with a `url` field, containing a string with the URL, or a `b64_json` field, containing a string with the base64-encoded string value for the image data.
+  public init(from decoder: Decoder) throws {
+    // get a keyed container
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    // try to decode the url
+    if let url = try? container.decode(URL.self, forKey: .url) {
+      self = .url(url)
+      return
+    }
+    // try to decode the base64
+    if let base64 = try? container.decode(String.self, forKey: .base64) {
+      self = .base64(Data(base64Encoded: base64)!)
+      return
+    }
+    // if we get here, we couldn't decode either
+    throw Client.Error.unexpectedResponse("Unknown response format for image.")
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    switch self {
+    case .url(let url):
+      try container.encode(url, forKey: .url)
+    case .base64(let data):
+      try container.encode(data.base64EncodedString(), forKey: .base64)
+    }
+  }
 }
--- Tests/OpenAIBitsTests/ImagesTests.swift
+++ Tests/OpenAIBitsTests/ImagesTests.swift
@@ -4,18 +4,49 @@ import CustomDump
 
 final class ImagesTests: XCTestCase {
 
-    func testImageToJSON() throws {
-      let value = Image.url("http://foo.bar")
+    func testImageDataURLToJSON() throws {
+      let value = Image.Data.url(URL(string: "http://foo.bar")!)
       XCTAssertNoDifference(
         #"{"url":"http://foo.bar"}"#,
         try jsonEncode(value, options: [.withoutEscapingSlashes]))
     }
   
-  func testImageFromJSON() throws {
-    let value: Image = try jsonDecode(#"{"url":"http://foo.bar"}"#)
+  func testImageDataURLFromJSON() throws {
+    let value: Image.Data = try jsonDecode(#"{"url":"http://foo.bar"}"#)
     XCTAssertNoDifference(
-      Image.url("http://foo.bar"),
+      Image.Data.url(URL(string: "http://foo.bar")!),
       value
+    )
+  }
+
+  func testImageDataBase64ToJSON() throws {
+    let data = "ABC".data(using: .utf8)!
+    let value = Image.Data.base64(data)
+    XCTAssertNoDifference(
+      #"{"base64":"QUJD"}"#,
+      try jsonEncode(value, options: [.withoutEscapingSlashes]))
+  }
+
+  func testImageDataBase64FromJSON() throws {
+    let data = "ABC".data(using: .utf8)!
+    let value: Image.Data = try jsonDecode(#"{"base64":"QUJD"}"#)
+    XCTAssertNoDifference(
+      Image.Data.base64(data),
+      value
+    )
+  }
+
+  func testImageToJSON() throws {
+    let now = Date(timeIntervalSince1970: 1589478378)
+    let value = Image(
+      created: now,
+      data: [.url(URL(string: "http://foo.bar")!)]
+    )
+    
+    XCTAssertNoDifference("""
+      {"created":1589478378,"data":[{"url":"http://foo.bar"}]}
+      """,
+      try jsonEncode(value, options: [.withoutEscapingSlashes])
     )
   }
 
