90fea8b9a119c69d48bb5b127ce440d96aa22acd
David Peterson <david@randombits.org>
2022-11-05 14:11:13 +1000

    Added support for `Images/Edits`

--- Sources/OpenAIBits/Calls/Images.swift
+++ Sources/OpenAIBits/Calls/Images.swift
@@ -1,3 +1,6 @@
+import Foundation
+import MultipartForm
+
 /// Given a prompt and/or an input image, the model will generate a new image.
 ///
 /// ## See Also
@@ -6,7 +9,25 @@
 /// - [Image Generation Guide](https://beta.openai.com/docs/guides/images)
 public enum Images { }
 
-// MARK: Generate
+// MARK: Support Types
+
+extension Images {
+  /// The options for image size generated.
+  public enum Size: String, Codable {
+    case of256x256 = "256x256"
+    case of512x512 = "512x512"
+    case of1024x1024 = "1024x1024"
+  }
+
+  /// The options for the response format.
+  public enum ResponseFormat: String, Codable {
+    case url
+    case base64 = "b64_json"
+  }
+}
+
+
+// MARK: Generations
 
 extension Images {
   /// A ``Call`` that creates an image given a prompt.
@@ -30,19 +51,6 @@ extension Images {
   public struct Generations: JSONPostCall {
     public typealias Response = Image
 
-    /// The options for image size generated.
-    public enum Size: String, Codable {
-      case of256x256 = "256x256"
-      case of512x512 = "512x512"
-      case of1024x1024 = "1024x1024"
-    }
-
-    /// The options for the response format.
-    public enum ResponseFormat: String, Codable {
-      case url
-      case base64 = "b64_json"
-    }
-
     public var path: String { "images/generations" }
 
     /// A text description of the desired image(s). The maximum length is 1000 characters.
@@ -51,10 +59,10 @@ extension Images {
     /// The number of images to generate. Must be between 1 and 10. (defaults to `1`)
     public let n: Int?
 
-    /// The size of the generated images. Must be one of ``Images/Generations/Size-swift.enum/of256x256``, ``Images/Generations/Size-swift.enum/of512x512``, or ``Images/Generations/Size-swift.enum/of1024x1024`` (the default).
+    /// The size of the generated images. Must be one of ``Images/Size-swift.enum/of256x256``, ``Images/Size-swift.enum/of512x512``, or ``Images/Size-swift.enum/of1024x1024`` (the default).
     public let size: Size?
 
-    /// The format in which the generated images are returned. Must be one of ``Images/Generations/ResponseFormat-swift.enum/url`` or ``Images/Generations/ResponseFormat-swift.enum/base64``.
+    /// The format in which the generated images are returned. Must be one of ``Images/ResponseFormat-swift.enum/url`` or ``Images/ResponseFormat-swift.enum/base64``.
     public let responseFormat: ResponseFormat?
 
     /// A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse.
@@ -65,15 +73,102 @@ extension Images {
     /// - Parameters:
     ///   - prompt: The prompt.
     ///   - n: The number of images. (defaults to `1`)
-    ///   - size: The size of the image (defaults to ``Images/Generations/Size-swift.enum/of1024x1024``)
-    ///   - responseFormat: <#responseFormat description#>
-    ///   - user: <#user description#>
+    ///   - size: The size of the image (defaults to ``Images/Size-swift.enum/of1024x1024``)
+    ///   - responseFormat: The ``Images/ResponseFormat``
+    ///   - user: A unique identifier representing your end-user.
     public init(prompt: String, n: Int? = nil, size: Size? = nil, responseFormat: ResponseFormat? = nil, user: String? = nil) {
       self.prompt = prompt
       self.n = n
       self.size = size
       self.responseFormat = responseFormat
       self.user = user
+    }
+  }
+}
+
+// MARK: Edits
+
+extension Images {
+  public struct Edits: MultipartPostCall {
+    /// Responds with an ``Image``.
+    public typealias Response = Image
+    
+    var path: String { "images/edits" }
+    
+    let boundary: String = UUID().uuidString
+    
+    /// The image to edit. Must be a valid PNG file, less than 4MB, and square.
+    public let image: URL
+    
+    /// An additional image whose fully transparent areas (e.g. where alpha is zero) indicate where image should be edited. Must be a valid PNG file, less than 4MB, and have the same dimensions as image.
+    public let mask: URL
+    
+    /// A text description of the desired image(s). The maximum length is 1000 characters.
+    public let prompt: String
+    
+    /// The number of images to generate. Must be between 1 and 10. (defaults to `1`)
+    public let n: Int?
+
+    /// The size of the generated images. Must be one of ``Images/Size-swift.enum/of256x256``, ``Images/Size-swift.enum/of512x512``, or ``Images/Size-swift.enum/of1024x1024`` (the default).
+    public let size: Size?
+
+    /// The format in which the generated images are returned. Must be one of ``Images/ResponseFormat-swift.enum/url`` or ``Images/ResponseFormat-swift.enum/base64``.
+    public let responseFormat: ResponseFormat?
+
+    /// A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse.
+    public let user: String?
+    
+    public init(
+      image: URL,
+      mask: URL,
+      prompt: String,
+      n: Int? = nil,
+      size: Size? = nil,
+      responseFormat: ResponseFormat? = nil,
+      user: String? = nil
+    ) {
+      self.image = image
+      self.mask = mask
+      self.prompt = prompt
+      self.n = n
+      self.size = size
+      self.responseFormat = responseFormat
+      self.user = user
+    }
+    
+    /// Returns a `MultipartForm`  based on the parameters.
+    ///
+    /// - Returns: The form.
+    public func getForm() throws -> MultipartForm {
+      let imageData = try Data(contentsOf: image)
+      let maskData = try Data(contentsOf: mask)
+      
+      var parts: [MultipartForm.Part] = [
+        .init(name: "image", data: imageData),
+        .init(name: "mask", data: maskData),
+        .init(name: "prompt", value: prompt),
+      ]
+      
+      if let n = n {
+        parts.append(.init(name: "n", value: String(describing: n)))
+      }
+      
+      if let size = size {
+        parts.append(.init(name: "size", value: size.rawValue))
+      }
+      
+      if let responseFormat = responseFormat {
+        parts.append(.init(name: "response_format", value: responseFormat.rawValue))
+      }
+      
+      if let user = user {
+        parts.append(.init(name: "user", value: user))
+      }
+      
+      return MultipartForm(
+        parts: parts,
+        boundary: boundary
+      )
     }
   }
 }
