2e0ee0d3b0dc77e4acce67260e4f733619e39e57
David Peterson <david@randombits.org>
2022-11-06 21:46:00 +1000

    Added `filename` to form file uploads.

--- Sources/OpenAIBits/Calls/Images.swift
+++ Sources/OpenAIBits/Calls/Images.swift
@@ -197,8 +197,8 @@ extension Images {
     /// - Returns: The form.
     public func getForm() throws -> MultipartForm {
       var parts: [MultipartForm.Part] = [
-        .init(name: "image", data: image, contentType: "application/octet-stream"),
-        .init(name: "mask", data: mask, contentType: "application/octet-stream"),
+        .init(name: "image", data: image, filename: "image.png", contentType: "image/png"),
+        .init(name: "mask", data: mask, filename: "mask.png", contentType: "image/png"),
         .init(name: "prompt", value: prompt),
       ]
       
@@ -301,7 +301,7 @@ extension Images {
     /// - Returns: The form.
     public func getForm() throws -> MultipartForm {
       var parts: [MultipartForm.Part] = [
-        .init(name: "image", data: image, contentType: "image/png"),
+        .init(name: "image", data: image, filename: "image.png", contentType: "image/png"),
       ]
       
       if let n = n {
